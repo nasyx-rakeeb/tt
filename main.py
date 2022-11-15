1687f8e590e4d7ecbce02cd8dc372e5e88b70a34
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-11-15 23:08:32 +0530

    hdusi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request 
 import datetime
-#from lunarcalendar import Converter, Solar, Lunar, DateNotExist
+from lunarcalendar import Converter, Solar, Lunar, DateNotExist
 
 today = datetime.date.today()
 
@@ -8,12 +8,12 @@ year = today.year
 month = today.month
 day = today.day
 
-#solar = Solar(year, month, day)
-#lunar = Converter.Solar2Lunar(solar)
+solar = Solar(year, month, day)
+lunar = Converter.Solar2Lunar(solar)
 
-#lunarYear = lunar.year 
-#lunarMonth = lunar.month
-#lunarDay = lunar.day
+lunarYear = lunar.year 
+lunarMonth = lunar.month
+lunarDay = lunar.day
 
 app = Flask(__name__) 
 
@@ -23,7 +23,7 @@ def helloworld():
 
     if(request.method == 'GET'): 
 
-        data = {"lunar_year": "test", "lunar_month": "test", "lunar_day": "test"} 
+        data = {"lunar_year": lunarYear, "lunar_month": lunarMonth, "lunar_day": lunarDay} 
 
         return jsonify(data) 
 
